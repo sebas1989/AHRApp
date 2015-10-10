ec40fc7e0842a043fae04721c0659fdfe4114767
sebastianmendezgiron <sevy2103@gmail.com>
2015-10-10 01:01:19 -0500

    Marcos pilotos2

---
+++ app/src/main/java/autodromo/punkmkt/com/ahrapp/GraciasParticiparActivity.java
@@ -0,0 +1,20 @@
+package autodromo.punkmkt.com.ahrapp;
+
+/**
+ * Created by sebastianmendezgiron on 09/10/15.
+ */
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class GraciasParticiparActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gracias_participar);
+    }
+
+}
---
+++ app/src/main/java/autodromo/punkmkt/com/ahrapp/ServiciosActivity.java
@@ -0,0 +1,16 @@
+package autodromo.punkmkt.com.ahrapp;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by sebastianmendezgiron on 09/10/15.
+ */
+public class ServiciosActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_servicios);
+    }
+}
